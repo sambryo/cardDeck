48d63787f927f4fc14fc44a76b404d42431ca3ce
samTer <samuelbryo@gmail.com>
2019-01-04 20:00:38 -0800

    created decks

--- deck.go
+++ deck.go
@@ -6,7 +6,26 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six",
+		"Seven", "eight", "nine", "ten",
+		"Jack", "kings", "Queen"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // A recieve on a function
+// Any variable of type "deck" now gets access to the "print" method
+// why d -> convention one or two letter of the type
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
